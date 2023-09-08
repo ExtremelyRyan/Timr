3926dcfd5e9f645a6f799d3016a0f3ad618ccdcd
Ryan Mason <ryan.mason814@gmail.com>
2023-09-08 06:39:44 -0700

    added calc_time_diff command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::path::PathBuf;
-use chrono::{self};
 
+use std::{path::PathBuf, time::SystemTime};
+use chrono::{NaiveTime, Utc, Local};
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
@@ -38,24 +38,41 @@ enum Commands {
         /// name of task 
         #[arg(required = true)]
         task: String,
+    },
+
+    /// get difference between two time inputs, seperated by a space 
+    Calc {
+        /// starting time 
+        #[arg(required = true)]
+        start_time: String,
+        /// ending time 
+        #[arg(required = false)]
+        ending_time: Option<String>,
     }
     
 }
 
+fn calc_time_diff(start_time: &str, end_time: &str) -> Result<String, anyhow::Error> { 
+    // let time_str = "22:10:57"; 
+    let start = NaiveTime::parse_from_str(start_time, "%H%M")?;
+    let end   = NaiveTime::parse_from_str(end_time  , "%H%M").unwrap_or(NaiveTime::from(Utc::now().time()));
+
+    dbg!(start, end); 
+
+    let hours = (end - start).num_hours();
+    let hours_in_min = hours * 60;
+    let minutes = (end - start).num_minutes() - hours_in_min; 
+
+
+    Ok(format!("{} hours, {} minutes", hours.abs(), minutes.abs()))
+}
+
 fn main() {
+    // let time_diff = testing("0645", "1600");
+    // if let Ok(td) = time_diff { println!("{td}"); }
+    
+
     let cli = Cli::parse(); 
-
-    // // You can check the value provided by positional arguments, or option arguments
-    // if let Some(name) = cli.name.as_deref() {
-    //     println!("Value for name: {name}");
-    // }
-
-    // if let Some(time) = cli.time {
-    //     println!("time entered: {}", time);
-    // }
-    // else {
-    //     println!("task {} started at: {}", cli.name.as_deref().unwrap() , chrono::offset::Local::now().time().format("%H:%M")); 
-    // }
 
     if let Some(config_path) = cli.config.as_deref() {
         println!("Value for config: {}", config_path.display());
@@ -63,10 +80,10 @@ fn main() {
 
     // You can see how many times a particular flag or argument occurred
     // Note, only flags can have multiple occurrences
-    match cli.debug {
-        false => println!("Debug mode is off"),
-        true => println!("Debug mode is on"), 
-    }
+    // match cli.debug {
+    //     false => println!("Debug mode is off"),
+    //     true => println!("Debug mode is on"), 
+    // }
 
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
@@ -84,10 +101,21 @@ fn main() {
                 false => println!("{} started at: {}", task , chrono::offset::Local::now().time().format("%H:%M")),
             }
             
-        }
+        },
         Some(Commands::End { task  }) => {
             println!("{} ended at: {}", task , chrono::offset::Local::now().time().format("%H:%M"));
+        },
+
+        Some(Commands::Calc { start_time, ending_time }) => {
+
+            let mut result: String = String::from("");
+            match ending_time {
+                Some(end) =>  result = calc_time_diff(start_time, end).unwrap(),
+                None => result = calc_time_diff(start_time, &format!("{:?}",SystemTime::now()).to_string() ).unwrap(),
+            } 
+            println!("{}", result);
         }
+
         
         None => {}
     }
