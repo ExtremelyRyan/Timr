ebfde1460e6573505af6f479293a9a71f0067c60
Ryan <ryan.mason814@gmail.com>
2023-09-08 12:02:54 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
-
-use std::{path::PathBuf, time::SystemTime};
-use chrono::{NaiveTime, Utc, Local};
+use chrono::NaiveTime;
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
-struct Cli { 
-    /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE")]
-    config: Option<PathBuf>,
-
-    /// Turn debugging information on 
+struct Cli {
+    /// Turn debugging information on
     #[arg(short, long)]
     debug: bool,
 
@@ -18,107 +12,98 @@ struct Cli {
     command: Option<Commands>,
 }
 
+struct Task {
+    date: String,
+    time_start: String,
+    time_end: Option<String>,
+    time_total: Option<String>,
+}
+
 #[derive(Subcommand)]
 enum Commands {
-    /// does testing things
-    Test {
-        /// lists test values 
-        list: bool,
-    },
     /// Start a task
+    // #[cmd(short, long)]
     Start {
         /// name of task  
         task: String,
         /// time started
         time: Option<String>,
-
     },
     /// End a task
-    End { 
-        /// name of task 
+    End {
+        /// name of task
         #[arg(required = true)]
         task: String,
     },
 
-    /// get difference between two time inputs, seperated by a space 
+    /// get difference between two time inputs, seperated by a space
     Calc {
-        /// starting time 
+        /// starting time: 24hr format (i.e 1630)
         #[arg(required = true)]
         start_time: String,
-        /// ending time 
+        /// optional ending time: 24hr format (i.e 1800)
         #[arg(required = false)]
         ending_time: Option<String>,
-    }
-    
-}
-
-fn calc_time_diff(start_time: &str, end_time: &str) -> Result<String, anyhow::Error> { 
-    // let time_str = "22:10:57"; 
-    let start = NaiveTime::parse_from_str(start_time, "%H%M")?;
-    let end   = NaiveTime::parse_from_str(end_time  , "%H%M").unwrap_or(NaiveTime::from(Utc::now().time()));
-
-    dbg!(start, end); 
-
-    let hours = (end - start).num_hours();
-    let hours_in_min = hours * 60;
-    let minutes = (end - start).num_minutes() - hours_in_min; 
-
-
-    Ok(format!("{} hours, {} minutes", hours.abs(), minutes.abs()))
+    },
 }
 
 fn main() {
-    // let time_diff = testing("0645", "1600");
-    // if let Ok(td) = time_diff { println!("{td}"); }
-    
-
-    let cli = Cli::parse(); 
-
-    if let Some(config_path) = cli.config.as_deref() {
-        println!("Value for config: {}", config_path.display());
-    }
-
-    // You can see how many times a particular flag or argument occurred
-    // Note, only flags can have multiple occurrences
-    // match cli.debug {
-    //     false => println!("Debug mode is off"),
-    //     true => println!("Debug mode is on"), 
-    // }
+    let cli = Cli::parse();
 
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
     match &cli.command {
-        Some(Commands::Test { list }) => {
-            if *list {
-                println!("Printing testing lists...");
-            } else {
-                println!("Not printing testing lists...");
-            }
+        Some(Commands::Start { task, time }) => match time.is_some() {
+            true => println!("{} started at: {}", task, time.as_deref().unwrap()),
+            false => println!(
+                "{} started at: {}",
+                task,
+                chrono::offset::Local::now().time().format("%H:%M")
+            ),
         },
-        Some(Commands::Start { task, time }) => {
-            match time.is_some() {
-                true  => println!("{} started at: {}", task , time.as_deref().unwrap()),
-                false => println!("{} started at: {}", task , chrono::offset::Local::now().time().format("%H:%M")),
-            }
-            
-        },
-        Some(Commands::End { task  }) => {
-            println!("{} ended at: {}", task , chrono::offset::Local::now().time().format("%H:%M"));
-        },
+        Some(Commands::End { task }) => {
+            println!(
+                "{} ended at: {}",
+                task,
+                chrono::offset::Local::now().time().format("%H:%M")
+            );
+        }
 
-        Some(Commands::Calc { start_time, ending_time }) => {
-
-            let mut result: String = String::from("");
-            match ending_time {
-                Some(end) =>  result = calc_time_diff(start_time, end).unwrap(),
-                None => result = calc_time_diff(start_time, &format!("{:?}",SystemTime::now()).to_string() ).unwrap(),
-            } 
+        Some(Commands::Calc {
+            start_time,
+            ending_time,
+        }) => {
+            let result: String = match ending_time {
+                Some(end) => calc_time_diff(start_time, end, &cli).unwrap(),
+                None => {
+                    let time = get_current_time().unwrap();
+                    calc_time_diff(start_time, time.as_str(), &cli).unwrap()
+                }
+            };
             println!("{}", result);
         }
 
-        
         None => {}
     }
+}
 
-    // Continued program logic goes here...
+fn calc_time_diff(start_time: &str, end_time: &str, cli: &Cli) -> Result<String, anyhow::Error> {
+    // let time_str = "22:10:57";
+    let start = NaiveTime::parse_from_str(start_time, "%H%M")?;
+    let end = NaiveTime::parse_from_str(end_time, "%H%M")?;
+
+    if cli.debug { println!("start: {}\t end {}",start, end);} 
+
+    let hours = (end - start).num_hours();
+    let hours_in_min = hours * 60;
+    let minutes = (end - start).num_minutes() - hours_in_min;
+
+    Ok(format!("{} hours, {} minutes", hours.abs(), minutes.abs()))
+}
+
+fn get_current_time() -> anyhow::Result<String> {
+    // get current time
+    let binding = chrono::Local::now().time().format("%H%M").to_string();
+
+    Ok(binding)
 }
